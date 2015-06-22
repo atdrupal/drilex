b9dd38e438d99418386f3e67081bd72a47ee6aec
Andy T. <andytruong@users.noreply.github.com>
2015-06-23 03:58:54 +0700

    Update App.php
    Custom template error handling.

--- src/App.php
+++ src/App.php
@@ -4,6 +4,8 @@ namespace vendor_name\project_name;
 
 use Silex\Application;
 use vendor_name\project_name\traits\GetSetTrait;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Symfony\Component\Debug\ExceptionHandler;
 
 class App extends Application
 {
@@ -16,6 +18,30 @@ class App extends Application
         
         $this->register(new ServiceProvider());
         $this->mount('/', new ControllerProvider());
+    }
+    
+    public function boot()
+    {
+        parent::boot();
+        
+        // Custom error handler page, it's must be placed before any route definition.
+        // Customize error template by error code by create template: pages/error/{404,500,..}.twig
+        $this->error(function (\Exception $e) {
+            $template = 'pages/error/default.twig';
+            $code = $e->getStatusCode();
+            $exception = FlattenException::create($e);
+            $handler = new ExceptionHandler($this['debug']);
+
+            if ($this->getTwig()->getLoader()->exists("pages/error/$code.twig")) {
+                $template = "pages/error/$code.twig";
+            }
+
+            return $this->getTwig()->render($template, [
+                'exception'  => $exception,
+                'stylesheet' => $handler->getStylesheet($exception),
+                'content'    => $handler->getContent($exception),
+            ]);
+        });
     }
 
 }
