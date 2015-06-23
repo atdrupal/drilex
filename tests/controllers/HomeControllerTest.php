991496aa38b0bd94e518c930f08f9184335a7a75
Andy T. <andytruong@users.noreply.github.com>
2015-06-23 08:49:10 +0700

    Create HomeControllerTest.php
    Add example controller test case.

---
+++ tests/controllers/HomeControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace vendor_name\project_name\test_cases\controllers;
+
+use Symfony\Component\HttpFoundation\Request;
+use vendor_name\project_name\test_cases\BaseTestCase;
+
+class HomeControllerTest extends BaseTestCase
+{
+
+    public function testActionGet()
+    {
+        $app = $this->getApplication();
+        $response = $app->handle(Request::create('/'));
+        $this->assertEquals('Welcome to <strong>Project Name</strong>!', $response->getContent());
+    }
+
+}
