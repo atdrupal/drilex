cde025e0dc78aa4fe0c60dadf2e3b61e7b101360
Andy Truong <thehongtt@gmail.com>
2015-07-20 16:16:10 +0700

    Remove drupal- prefix on routes.

--- src/DrupalControllerProvider.php
+++ src/DrupalControllerProvider.php
@@ -21,10 +21,10 @@ class DrupalControllerProvider implements ControllerProviderInterface
         $route = $app['controllers_factory'];
 
         $route->get('/user', 'ctr.drupal:action');
-        $route->match('/user/login', 'ctr.drupal:action')->method('GET|POST')->bind('drupal-user-login');
-        $route->get('/user/password', 'ctr.drupal:action')->method('GET|POST')->bind('drupal-user-password');
-        $route->get('/user/logout', 'ctr.drupal:actionGetLogout')->method('GET')->bind('drupal-user-logout');
-        $route->match('/user/{uid}/edit', 'ctr.drupal:action')->method('GET|POST')->bind('drupal-user-edit');
+        $route->match('/user/login', 'ctr.drupal:action')->method('GET|POST')->bind('user-login');
+        $route->get('/user/password', 'ctr.drupal:action')->method('GET|POST')->bind('user-password');
+        $route->get('/user/logout', 'ctr.drupal:actionGetLogout')->method('GET')->bind('user-logout');
+        $route->match('/user/{uid}/edit', 'ctr.drupal:action')->method('GET|POST')->bind('user-edit');
         $route->get('/{type}/{id}', 'ctr.drupal:actionGetEntity')->bind('drupal-entity');
 
         return $route;
